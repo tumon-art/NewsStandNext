37183ffbf12417cf9f6f0266e696e1262126b72a
tumon <tumonkhn@gmail.com>
2022-10-09 12:11:42 +0600

    added env

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  // env: {
+  //   SERVER: "https://newsstand.fly.dev/",
+  // },
+
+  // https://news-stand-server.herokuapp.com/
+  // https://evening-stream-93162.herokuapp.com/
+  // http://localhost:3001/
+  // https://newsstand.fly.dev/
+  // nothing
+};
+
+module.exports = nextConfig;
